3ca3c7279f51f8a37f4bd186b2bde17059e08fbe
Jeffrey Cheung <jeffreyy.cheungg@gmail.com>
2018-01-26 16:28:35 -0500

    draggable installed

--- components/Lobby.js
+++ components/Lobby.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
-const Lobby = () => (
+const Lobby = ({navigation}) => (
   <View style={styles.container}>
     <Text>This is my Lobby Component!</Text>
+    <Button
+            onPress={() => navigation.navigate('GameRoom')}
+            title="Start Game"
+        />
   </View>
 );
 
