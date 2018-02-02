55a5e3e061d6e20e84b219f26c7c43839fcfe287
Jimmi Chen <gchen20@u.rochester.edu>
2018-02-02 16:33:28 -0500

    socket pass hand attempt

--- server/store/index.js
+++ server/store/index.js
@@ -1,15 +1,12 @@
 const createStore = require('redux').createStore
 const combineReducers = require('redux').combineReducers
 const applyMiddleware = require('redux').applyMiddleware
-const passHand = require('./reducers/endTurn')
+const endTurn = require('./reducers/endTurn')
 
 
-const reducer = combineReducers({ passHand });
+const reducer = combineReducers({ endTurn });
 
 const store = createStore(reducer);
 
-// export default store;
 
-// export * from './reducers/endTurn';
-
-module.exports = { store, passHand }
+module.exports = { store, endTurn }
